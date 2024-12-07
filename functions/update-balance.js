c419d0ce3e3bc1a8856d290de063a3eeda3acfe0
Ruben Contreras <44092831+racmark76@users.noreply.github.com>
2024-12-06 20:46:48 -0500

    Update update-balance.js

--- functions/update-balance.js
+++ functions/update-balance.js
@@ -48,6 +48,7 @@ exports.handler = async (event) => {
     await base('Gift Cards').update(record.id, {
       'Current Balance': newBalance,
       'Status': newBalance <= 0 ? 'Depleted' : 'Active'
+      'Last Used Date': new Date().toLocaleDateString('en-US')
   
     });
 
