14f7c2b94d79c959b7ceff192a0bef9277f8bbd3
Ruben Contreras <44092831+racmark76@users.noreply.github.com>
2025-01-01 15:11:20 -0500

    Update update-addons.js

--- functions/update-addons.js
+++ functions/update-addons.js
@@ -1,5 +1,4 @@
 
-
 const Airtable = require('airtable');
 
 const base = new Airtable({
@@ -19,43 +18,55 @@ exports.handler = async (event, context) => {
 
   try {
     const data = JSON.parse(event.body)[0];
-    const matches = [];
+    const { customerInfo, orderDetails } = data;
 
-    // Get all records for this customer
+    console.log('Received add-ons:', orderDetails.addOns);
+
+    // Get customer records
     const records = await base('tblM6K7Ii11HBkrW9').select({
-      filterByFormula: `{Customer Name} = '${data.customerInfo.name}'`
+      filterByFormula: `{Customer Name} = '${customerInfo.name}'`
     }).all();
 
-    // Match records with add-ons
-    records.forEach(record => {
-      const recordDay = record.get('Day');
-      const dayCapitalized = recordDay.charAt(0).toUpperCase() + recordDay.slice(1);
-      
-      if (data.orderDetails.addOns[dayCapitalized]) {
-        matches.push({
-          recordId: record.id,
-          addOns: data.orderDetails.addOns[dayCapitalized]
-        });
-      }
-    });
+    console.log('Found records:', records.map(r => ({
+      day: r.get('Day'),
+      week: r.get('Week')
+    })));
 
-    // Update matched records
-    const updates = await Promise.all(matches.map(match => {
-      const extrasString = match.addOns
-        .map(addon => `${addon.name} (${addon.quantity})`)
-        .join(', ');
+    const updates = [];
+    for (const record of records) {
+      const recordDay = record.get('Day');
+      console.log('Processing day:', recordDay);
+      
+      const dayCapitalized = recordDay.charAt(0).toUpperCase() + recordDay.slice(1);
+      console.log('Looking for add-ons for:', dayCapitalized);
+      
+      const dayAddOns = orderDetails.addOns[dayCapitalized] || [];
+      console.log('Found add-ons:', dayAddOns);
+
+      if (dayAddOns.length > 0) {
+        const extrasString = dayAddOns.map(addon => 
+          `${addon.name} (${addon.quantity})`
+        ).join(', ');
         
-      return base('tblM6K7Ii11HBkrW9').update(match.recordId, {
-        'Extras': extrasString
-      });
-    }));
+        console.log('Updating with:', extrasString);
+
+        const updated = await base('tblM6K7Ii11HBkrW9').update(record.id, {
+          'Extras': extrasString
+        });
+        updates.push(updated);
+      } else {
+        console.log('No add-ons found for this day');
+      }
+    }
 
     return {
       statusCode: 200,
       headers,
       body: JSON.stringify({
         success: true,
-        updates: updates.length
+        updates: updates.length,
+        addOns: orderDetails.addOns,
+        processedDays: records.map(r => r.get('Day'))
       })
     };
 
@@ -66,8 +77,88 @@ exports.handler = async (event, context) => {
       headers,
       body: JSON.stringify({
         success: false,
-        error: error.message
+        error: error.message,
+        stack: error.stack
       })
     };
   }
 };
+
+
+
+
+
+
+
+
+// const Airtable = require('airtable');
+
+// const base = new Airtable({
+//   apiKey: 'patySI8tdVaCy75dA.9e891746788af3b4420eb93e6cd76d866317dd4950b648196c88b0d9f0d51cf3'
+// }).base('appYJ9gWRBFOLfb0r');
+
+// exports.handler = async (event, context) => {
+//   const headers = {
+//     'Access-Control-Allow-Origin': 'https://bot.eitanerd.com',
+//     'Access-Control-Allow-Headers': 'Content-Type',
+//     'Access-Control-Allow-Methods': 'POST, OPTIONS'
+//   };
+
+//   if (event.httpMethod === 'OPTIONS') {
+//     return { statusCode: 200, headers, body: '' };
+//   }
+
+//   try {
+//     const data = JSON.parse(event.body)[0];
+//     const matches = [];
+
+//     // Get all records for this customer
+//     const records = await base('tblM6K7Ii11HBkrW9').select({
+//       filterByFormula: `{Customer Name} = '${data.customerInfo.name}'`
+//     }).all();
+
+//     // Match records with add-ons
+//     records.forEach(record => {
+//       const recordDay = record.get('Day');
+//       const dayCapitalized = recordDay.charAt(0).toUpperCase() + recordDay.slice(1);
+      
+//       if (data.orderDetails.addOns[dayCapitalized]) {
+//         matches.push({
+//           recordId: record.id,
+//           addOns: data.orderDetails.addOns[dayCapitalized]
+//         });
+//       }
+//     });
+
+//     // Update matched records
+//     const updates = await Promise.all(matches.map(match => {
+//       const extrasString = match.addOns
+//         .map(addon => `${addon.name} (${addon.quantity})`)
+//         .join(', ');
+        
+//       return base('tblM6K7Ii11HBkrW9').update(match.recordId, {
+//         'Extras': extrasString
+//       });
+//     }));
+
+//     return {
+//       statusCode: 200,
+//       headers,
+//       body: JSON.stringify({
+//         success: true,
+//         updates: updates.length
+//       })
+//     };
+
+//   } catch (error) {
+//     console.error('Error:', error);
+//     return {
+//       statusCode: 500,
+//       headers,
+//       body: JSON.stringify({
+//         success: false,
+//         error: error.message
+//       })
+//     };
+//   }
+// };
