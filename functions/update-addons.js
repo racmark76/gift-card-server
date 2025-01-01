ef9344c96153c91d6e764a5b6811601fa0b359c9
Ruben Contreras <44092831+racmark76@users.noreply.github.com>
2025-01-01 14:07:17 -0500

    Update update-addons.js

--- functions/update-addons.js
+++ functions/update-addons.js
@@ -1,4 +1,5 @@
 
+
 const Airtable = require('airtable');
 
 const base = new Airtable({
@@ -18,38 +19,43 @@ exports.handler = async (event, context) => {
 
   try {
     const data = JSON.parse(event.body)[0];
-    const { customerInfo, orderDetails } = data;
-    const addOns = orderDetails.addOns;
+    const matches = [];
 
-    // Get all customer records
+    // Get all records for this customer
     const records = await base('tblM6K7Ii11HBkrW9').select({
-      filterByFormula: `{Customer Name} = '${customerInfo.name}'`
+      filterByFormula: `{Customer Name} = '${data.customerInfo.name}'`
     }).all();
 
-    const updates = [];
-    for (const record of records) {
+    // Match records with add-ons
+    records.forEach(record => {
       const recordDay = record.get('Day');
       const dayCapitalized = recordDay.charAt(0).toUpperCase() + recordDay.slice(1);
       
-      // Check if there are add-ons for this day
-      const dayAddOns = addOns[dayCapitalized + 'Array']?.[0]?.['1Collection'];
-      
-      if (dayAddOns) {
-        const extrasString = `${dayAddOns.name} (${dayAddOns.quantity})`;
-        
-        const updated = await base('tblM6K7Ii11HBkrW9').update(record.id, {
-          'Extras': extrasString
+      if (data.orderDetails.addOns[dayCapitalized]) {
+        matches.push({
+          recordId: record.id,
+          addOns: data.orderDetails.addOns[dayCapitalized]
         });
-        updates.push(updated);
       }
-    }
+    });
+
+    // Update matched records
+    const updates = await Promise.all(matches.map(match => {
+      const extrasString = match.addOns
+        .map(addon => `${addon.name} (${addon.quantity})`)
+        .join(', ');
+        
+      return base('tblM6K7Ii11HBkrW9').update(match.recordId, {
+        'Extras': extrasString
+      });
+    }));
 
     return {
       statusCode: 200,
       headers,
       body: JSON.stringify({
         success: true,
-        updatedRecords: updates.length
+        updates: updates.length
       })
     };
 
@@ -65,78 +71,3 @@ exports.handler = async (event, context) => {
     };
   }
 };
-
-
-
-
-// const Airtable = require('airtable');
-
-// const base = new Airtable({
-//   apiKey: 'patySI8tdVaCy75dA.9e891746788af3b4420eb93e6cd76d866317dd4950b648196c88b0d9f0d51cf3'
-// }).base('appYJ9gWRBFOLfb0r');
-
-// exports.handler = async (event, context) => {
-//   const headers = {
-//     'Access-Control-Allow-Origin': 'https://bot.eitanerd.com',
-//     'Access-Control-Allow-Headers': 'Content-Type',
-//     'Access-Control-Allow-Methods': 'POST, OPTIONS'
-//   };
-
-//   if (event.httpMethod === 'OPTIONS') {
-//     return { statusCode: 200, headers, body: '' };
-//   }
-
-//   try {
-//     const data = JSON.parse(event.body)[0];
-//     const matches = [];
-
-//     // Get all records for this customer
-//     const records = await base('tblM6K7Ii11HBkrW9').select({
-//       filterByFormula: `{Customer Name} = '${data.customerInfo.name}'`
-//     }).all();
-
-//     // Match records with add-ons
-//     records.forEach(record => {
-//       const recordDay = record.get('Day');
-//       const dayCapitalized = recordDay.charAt(0).toUpperCase() + recordDay.slice(1);
-      
-//       if (data.orderDetails.addOns[dayCapitalized]) {
-//         matches.push({
-//           recordId: record.id,
-//           addOns: data.orderDetails.addOns[dayCapitalized]
-//         });
-//       }
-//     });
-
-//     // Update matched records
-//     const updates = await Promise.all(matches.map(match => {
-//       const extrasString = match.addOns
-//         .map(addon => `${addon.name} (${addon.quantity})`)
-//         .join(', ');
-        
-//       return base('tblM6K7Ii11HBkrW9').update(match.recordId, {
-//         'Extras': extrasString
-//       });
-//     }));
-
-//     return {
-//       statusCode: 200,
-//       headers,
-//       body: JSON.stringify({
-//         success: true,
-//         updates: updates.length
-//       })
-//     };
-
-//   } catch (error) {
-//     console.error('Error:', error);
-//     return {
-//       statusCode: 500,
-//       headers,
-//       body: JSON.stringify({
-//         success: false,
-//         error: error.message
-//       })
-//     };
-//   }
-// };
