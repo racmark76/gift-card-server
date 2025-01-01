c25d46e47c3ebea136882ab05d71fb7d0ea62fde
Ruben Contreras <44092831+racmark76@users.noreply.github.com>
2025-01-01 13:56:26 -0500

    Update update-addons.js

--- functions/update-addons.js
+++ functions/update-addons.js
@@ -18,34 +18,24 @@ exports.handler = async (event, context) => {
 
   try {
     const data = JSON.parse(event.body)[0];
-    console.log('Processing data for:', data.customerInfo.name);
+    const { customerInfo, orderDetails } = data;
+    const addOns = orderDetails.addOns;
 
-    // First get all records for this customer
+    // Get all customer records
     const records = await base('tblM6K7Ii11HBkrW9').select({
-      filterByFormula: `{Customer Name} = '${data.customerInfo.name}'`
+      filterByFormula: `{Customer Name} = '${customerInfo.name}'`
     }).all();
 
-    console.log('Found records:', records.map(r => ({
-      day: r.get('Day'),
-      week: r.get('Week')
-    })));
-
-    // Process each order
     const updates = [];
     for (const record of records) {
       const recordDay = record.get('Day');
       const dayCapitalized = recordDay.charAt(0).toUpperCase() + recordDay.slice(1);
       
-      console.log('Checking addons for day:', dayCapitalized);
-      console.log('Available addons:', Object.keys(data.orderDetails.addOns));
+      // Check if there are add-ons for this day
+      const dayAddOns = addOns[dayCapitalized + 'Array']?.[0]?.['1Collection'];
       
-      const dayAddOns = data.orderDetails.addOns[dayCapitalized] || [];
-      if (dayAddOns.length > 0) {
-        const extrasString = dayAddOns.map(addon => 
-          `${addon.name} (${addon.quantity})`
-        ).join(', ');
-        
-        console.log(`Updating ${recordDay} with:`, extrasString);
+      if (dayAddOns) {
+        const extrasString = `${dayAddOns.name} (${dayAddOns.quantity})`;
         
         const updated = await base('tblM6K7Ii11HBkrW9').update(record.id, {
           'Extras': extrasString
@@ -59,9 +49,7 @@ exports.handler = async (event, context) => {
       headers,
       body: JSON.stringify({
         success: true,
-        updates: updates.length,
-        processed: records.length,
-        availableDays: Object.keys(data.orderDetails.addOns)
+        updatedRecords: updates.length
       })
     };
 
@@ -77,8 +65,6 @@ exports.handler = async (event, context) => {
     };
   }
 };
-
-
 
 
 
